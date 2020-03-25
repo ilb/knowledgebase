920d60d4d06303e5297b4b9c147d70d094e234e1
Vlad <vlad.gudov@inbox.ru>
2020-03-25 16:47:06 +0400

    Структура, начало верстки

--- web/DocumentList.php
+++ web/DocumentList.php
@@ -1,6 +1,29 @@
 <?php
+require_once '../web/template/header.php';
+    /*
+     * Выводит список всех документов
+     */
+    ini_set("error_reporting", E_ALL);
+    ini_set("display_errors", 1);
+    ini_set("display_startup_errors", 1);
 
-/* 
- * Выводит список всех документов
- */
+    require_once '../config/bootstrap.php';
 
+    $catalog = new \catalog\Catalog("../web/index.html");
+//    $catalog->createDocuments();
+?>
+    <h1>Список документов</h1>
+<?php
+    foreach ($catalog->getDocuments() as $document):
+        $document->createResources();
+        foreach ($document->getResources() as $resource) :
+?>
+    
+    <span class="document"><?=$document->getName()?></span>
+    
+<?php
+        endforeach;
+    endforeach;
+?>
+</body>
+</html>
---
+++ web/template/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>DocumentList</title>
+        <link rel="stylesheet" href="css/main.css"/>
+        <link rel="shortcut icon" href="assets/img/pCIQeJeeRck.png" type="image/png">
+    </head>
+    <body>
+        <header>
+            <img alt="Logo" src="assets/img/pCIQeJeeRck.png">
+
+            <nav>
+                <h2> %Пользователь% </h2>
+                <div class="search">
+                    <input type="text" class="search" placeholder="Введите ключевое слово">
+                    <img alt="Поиск" src="assets/img/search.png">
+                </div>  
+                
+            </nav>
+        </header>
