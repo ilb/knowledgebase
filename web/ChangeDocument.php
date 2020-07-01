18bf8ff7f0908b3fe6add08ff8c3d87d21b23178
Vlad <vlad.gudov@inbox.ru>
2020-07-01 11:34:29 +0400

    Страница для изменения адреса документов

--- web/ChangeDocument.php
+++ web/ChangeDocument.php
@@ -3,6 +3,8 @@
  * Description Меняет ссылку документа
  */
 
+use usecase\document\DocumentList;
+
 require_once '../config/bootstrap.php';
 
 header("Content-type: text/xml");
@@ -10,7 +12,23 @@ echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1 plus MathML 2.0 plus SVG 1.1/
 "http://www.w3.org/2002/04/xhtml-math-svg/xhtml-math-svg-flat.dtd">
 <?xml-stylesheet type="text/xsl" href="css/main.xsl"?>';
 
+if (isset($_POST['document'])) {
+    if (empty(trim($_POST['urlAddr']))) {
+        $result = "Ключевое слово не может быть пустым";
+    } else {
+        // Добавление нового адреса
+
+        $result = "Произошла ошибка";
+        /*if ($res->execute()) {
+            $result = "Ключевое слово добавлено";
+        }
+        */
+    }
+}
+
 // Нужно переделать БД чтобы ссылка на документ менялась с сылкой на ресурс
+$documentList = new DocumentList("../web/index.html");
+$catalog = $documentList->execute();
 ?>
 
 <html xmlns="http://www.w3.org/1999/xhtml" lang="ru">
@@ -21,9 +39,39 @@ echo '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1 plus MathML 2.0 plus SVG 1.1/
 </head>
 
 <body>
-    <div class="container text">
-
-    </div>
+<div class="container text">
+    <?php
+    if (isset($result)):
+        ?>
+        <h1><?= $result ?></h1>
+    <?php
+    endif;
+    ?>
+    <form action="AddTag.php" method="post">
+        <fieldset>
+            <legend>Изменения</legend>
+            <div>
+                <label>
+                    Новый URL адрес для документа
+                    <input type="text" name="urlAddr" placeholder="Введите URL" style="width: 25%"/>
+                </label>
+                <label>
+                    Документ
+                    <select name="document" id="doc">
+                        <?php
+                        foreach ($catalog->getDocuments() as $document):
+                            ?>
+                            <option value="<?= $document->getName() ?>"><?= $document->getName() ?></option>
+                        <?php
+                        endforeach;
+                        ?>
+                    </select>
+                </label>
+                <button>Добавить</button>
+            </div>
+        </fieldset>
+    </form>
+</div>
 </body>
 
 </html>
