ce5f207065d6b3ed11e4ccb588defe6210778595
Gudov Vladislav <vlad.gudov@inbox.ru>
2020-08-12 09:01:00 +0400

    Schema update

--- web/test.php
+++ web/test.php
@@ -1,57 +1,8 @@
 <?php
-use Symfony\Component\Serializer\Encoder\JsonEncoder;
-use Symfony\Component\Serializer\Encoder\XmlEncoder;
-use Symfony\Component\Serializer\Normalizer\ObjectNormalizer;
-use Symfony\Component\Serializer\Serializer;
-use usecase\catalog\GetCatalog;
-
 require_once '../config/bootstrap.php';
 
-$encoders = [new XmlEncoder(), new JsonEncoder()];
-$normalizers = [new ObjectNormalizer()];
-
-$serializer = new Serializer($normalizers, $encoders);
-
-$documentList = new GetCatalog("../web/index.html");
-$catalog = $documentList->execute();
-foreach ($catalog->getDocuments() as $document) {
-    $document->createResources();
+$hreq = new HTTP_Request2Xml("schemas/command.xsd", null, "LoginRequest");
+if (!$hreq->isEmpty()) {
+    $hreq->validate();
+    $xml = $hreq->getAsXML();
 }
-// сериализованный класс
-// ради теста пока будет тут
-// сразу как получится вынесу в отдельную папку
-class Response {
-    private $elements;
-
-    function __construct($elem) {
-        $this->elements = $elem;
-    }
-
-    function setElement($elem) {
-        $this->elements = $elem;
-    }
-
-    function getElements() {
-        return $this->elements;
-    }
-
-}
-
-$res = new Response($catalog);
-$xml = $serializer->serialize($res, "xml");
-
-header("Content-type: text/xml");
-// это костыль я просто пока не разобрался как в сериализаторе прикрепить стили сразу
-$arr = explode("\n", $xml);
-$xml = $arr[0] . '<?xml-stylesheet type="text/xsl" href="stylesheets/DocumentList/DocumentList.xsl"?>'
-    . $arr[1];
-//echo  $xml ;
-
-//XML_Output::tryHTML($xml);
-XML_Output::tryHTML($xml,TRUE);
-
-/**
- * TODO:
- *  Реализовать класс по сериализации в json, xml, xhtml
- *  + Прикрепить стили
- */
