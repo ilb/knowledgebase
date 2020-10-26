ee7fe5561614b23e2b7532ea13325789727fe220
Gudov Vladislav <vlad.gudov@inbox.ru>
2020-10-26 17:01:16 +0400

    Factory

---
+++ classes/vcsclient/VCSClientFactory.php
@@ -0,0 +1,55 @@
+<?php
+
+
+namespace vcsclient;
+
+
+use config\Config;
+
+class VCSClientFactory {
+
+    /**
+     * @const array
+     */
+    const dirs = [ ".git", ".svn" ];
+
+    /**
+     * @var string
+     */
+    private $repo;
+
+    /**
+     * VCSClientFactory constructor.
+     * @param $repo
+     */
+    public function __construct($repo){
+        $this->repo = $repo;
+    }
+
+    /**
+     * Возвращает имя контроля версии или null в случае его отсутвия
+     * @return string|null
+     */
+    public function check() {
+        $files = scandir(Config::getInstance()->filespath . "/" . $this->repo);
+        foreach (self::dirs as $dir) {
+            if (in_array($dir, $files)) {
+                return substr($dir, 1);
+            }
+        }
+        return NULL;
+    }
+
+    /**
+     * Из ходя из версии отдает объект класса или null в случае отсутвия контроля версий
+     * @return VCSClientSVN|null
+     */
+    public function getVCSClient() {
+        switch ($this->repo) {
+            case "svn":
+                return new VCSClientSVN(Config::getInstance()->filespath . "/" . $this->repo);
+            default:
+                return NULL;
+        }
+    }
+}
