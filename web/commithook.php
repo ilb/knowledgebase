4b2b314b61917c0570f7b9380175467540e7ec77
Gudov Vladislav <vlad.gudov@inbox.ru>
2020-11-03 07:08:49 +0400

    diff create

--- web/commithook.php
+++ web/commithook.php
@@ -18,15 +18,18 @@ if (!$hreq->isEmpty()) {
 
 if ( $_SERVER["REQUEST_METHOD"] == "POST" ) {
     $diff = file_get_contents("php://input");
-    $repo = new Repository(Config::getInstance()->connection);
-    $pars = new SVNParser($diff);
-    $result = $pars->getEvent();
-    $data = $pars->getData();
-    $result = $pars->merge($result, $data);
-    unset($data, $pars);
-    $notify = new Notificate($result);
-    $notify->setRepository($repo);
-    $notify->execute();
+    
+//    $repo = new Repository(Config::getInstance()->connection);
+//    $pars = new SVNParser($diff);
+//    $result = $pars->getEvent();
+//    $data = $pars->getData();
+//    $result = $pars->merge($result, $data);
+//    unset($data, $pars);
+//    $notify = new Notificate($result);
+//    $notify->setRepository($repo);
+//    $notify->execute();
+    mail("gudov@bystrobank.ru", "База знаний", $diff, "Content-type: text/plain; charset=utf-8");
+
 }
 
 $VCSClientFactory = new VCSClientFactory(Config::getInstance()->filespath);
