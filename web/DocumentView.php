368f942aa963c3df3c3d300fc8a5a932624602e8
Gudov Vladislav <vlad.gudov@inbox.ru>
2021-01-11 12:32:46 +0400

    mime type class

--- web/DocumentView.php
+++ web/DocumentView.php
@@ -32,60 +32,62 @@ $login = Config::getInstance()->login;
 
 preg_match_all("/[a-z]+.*[a-z]+/", $doc, $mathes);
 $path = Config::getInstance()->filespath . "/" . $mathes[0][0];
-
+$type = explode(".", $path);
 $docContext = file_get_contents($path);
-header("Content-type: text/xml");
+$isImage = false;
+
+$mimeType = new Mime_Type();
+$mimeType = $Mime_Type->getTypeByContentAndExt($path, $type);
 // TODO:для отображения картинок
 $typesImage = ["png", "jpg", "jpeg", "gif"];
-$type = explode(".", $path);
 foreach ($typesImage as $value) {
-    if ($type[count($type)-1] == $value) {
-        header("Content-type: image/*");
-        echo $docContext;
-        exit(0);
+    if ($type[count($type) - 1] == $value) {
+        $isImage = true;
     }
 }
+if (!$isImage) {
+    $repo = new Repository(Config::getInstance()->connection);
+    $subs = new GetSubscriptionDocUser(Config::getInstance()->login, $doc);
+    $subs->setRepository($repo);
+    $subs = $subs->execute();
+    $ser = new Serialize();
 
-$repo = new Repository(Config::getInstance()->connection);
-$subs = new GetSubscriptionDocUser(Config::getInstance()->login, $doc);
-$subs->setRepository($repo);
-$subs = $subs->execute();
-$ser = new Serialize();
+    $subs = explode("<?xml version=\"1.0\"?>", $ser->arrToXML([]))[1];
 
-$subs = explode("<?xml version=\"1.0\"?>" , $ser->arrToXML([]))[1];
-
-if (!strpos($docContext, "oooxhtml.xsl")) {
-    $head = "<?xml-stylesheet type=\"text/xsl\" href=\"/oooxhtml/oooxhtml.xsl\"?>";
-    // убрать стили
-    $docContext = str_replace("<link rel=\"stylesheet\" type=\"text/css\" href=\"/oooxhtml/oooxhtml.css\"/>", "", $docContext);
-    // убрать скрипты
-    $docContext = str_replace("<script type=\"text/javascript\" src=\"/privilegedAPI/web/scripts/privilegedAPI.js\"></script>
+    if (!strpos($docContext, "oooxhtml.xsl")) {
+        $head = "<?xml-stylesheet type=\"text/xsl\" href=\"/oooxhtml/oooxhtml.xsl\"?>";
+        // убрать стили
+        $docContext = str_replace("<link rel=\"stylesheet\" type=\"text/css\" href=\"/oooxhtml/oooxhtml.css\"/>", "", $docContext);
+        // убрать скрипты
+        $docContext = str_replace("<script type=\"text/javascript\" src=\"/privilegedAPI/web/scripts/privilegedAPI.js\"></script>
         <script type=\"text/javascript\" src=\"/oooxhtml/oooxhtml.js\"></script>", "", $docContext);
-    // подключить свои стили
-    $d = strpos($docContext, "<html");
-    $docContext = substr($docContext, 0, $d) . $head . substr($docContext, $d);
-}
+        // подключить свои стили
+        $d = strpos($docContext, "<html");
+        $docContext = substr($docContext, 0, $d) . $head . substr($docContext, $d);
+    }
 
-$repoDocs = explode("/", $doc);
-$vcsFactory = new VCSClientFactory(Config::getInstance()->filespath);
-$vcsClient = $vcsFactory->getVCSClient($repoDocs[0]);
-$editURL = $vcsClient->info(implode("/", array_slice($repoDocs, 1)));
-$editURL = str_replace($_SERVER['ru.bystrobank.apps.svn.ws'], $_SERVER['ru.bystrobank.apps.svn.ws2'], $editURL);
+    $repoDocs = explode("/", $doc);
+    $vcsFactory = new VCSClientFactory(Config::getInstance()->filespath);
+    $vcsClient = $vcsFactory->getVCSClient($repoDocs[0]);
+    $editURL = $vcsClient->info(implode("/", array_slice($repoDocs, 1)));
+    $editURL = str_replace($_SERVER['ru.bystrobank.apps.svn.ws'], $_SERVER['ru.bystrobank.apps.svn.ws2'], $editURL);
 
 //$docContext = str_replace("href=\"/oooxhtml/", "href=\"oooxhtml/", $docContext);
-$d = strpos($docContext, "</body>");
-$dir = explode("/", $allName);
-$dir = array_slice($dir, 0, count($dir)-1);
-$dir = implode("/", $dir);
-$dop = "<file style='display: none'>$allName</file>" .
-    "<mainDir style='display: none'>$dir</mainDir>" .
-    "<document style='display: none'>$doc</document>" .
-    "<user style='display: none'>$login</user>" .
-    "<editURL style='display:none'>". $editURL[0] . "</editURL>" 
-    . $subs;
+    $d = strpos($docContext, "</body>");
+    $dir = explode("/", $allName);
+    $dir = array_slice($dir, 0, count($dir) - 1);
+    $dir = implode("/", $dir);
+    $dop = "<file style='display: none'>$allName</file>" .
+            "<mainDir style='display: none'>$dir</mainDir>" .
+            "<document style='display: none'>$doc</document>" .
+            "<user style='display: none'>$login</user>" .
+            "<editURL style='display:none'>" . $editURL[0] . "</editURL>"
+            . $subs;
 
-$docContext = substr($docContext, 0, $d) . $dop . substr($docContext, $d);
-header("Content-type: text/xml");
+    $docContext = substr($docContext, 0, $d) . $dop . substr($docContext, $d);
+}
+
+header("Content-type: $mimeType");
 echo $docContext;
 // если использовать это ломаются скрипты oooxhtml
 //XML_Output::tryHTML($docContext,TRUE);
