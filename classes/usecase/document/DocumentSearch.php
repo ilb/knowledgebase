9cf7aaf8ab9a134e64dc8295b063e46a033627be
Gudov Vladislav <vlad.gudov@inbox.ru>
2021-01-21 17:36:31 +0400

    #3 Поиск документов

--- classes/usecase/document/DocumentSearch.php
+++ classes/usecase/document/DocumentSearch.php
@@ -3,13 +3,14 @@
 /**
  * Поиск по документам тут
  */
+
 namespace usecase\document;
 
 use usecase\helper\UseCase;
 use requests\Curl;
 use parser\DocumentParser;
 
-class DocumentSearch extends UseCase  {
+class DocumentSearch extends UseCase {
 
     /**
      * @var string
@@ -47,51 +48,58 @@ class DocumentSearch extends UseCase  {
         //$repos = $pars->getRepos($this->dir); было для поиска по репозиториям
         $curl = new Curl("");
 //        foreach ($repos as $repo) {
-            $arr = [];
-            $curl->setURL(
+        $curl->setURL(
                 $this->source .
                 "/_search"
-            );
-            $data = [
-                "query"=> [
-                    "match"=> [
-                        "content"=> $this->keyWord,
-                    ],
+        );
+        $data = [
+            "query" => [
+                "match" => [
+                    "content" => $this->keyWord,
                 ],
-                "highlight"=> [
-                      "fragment_size" => 150,
-                      "fields"=> [
-                           "content"=> [],
-                      ],
-
+            ],
+            "highlight" => [
+                "fragment_size" => 150,
+                "fields" => [
+                    "content" => [],
                 ],
-            ];
-            $curl->setData($data);
-            $temp = $curl->getWithData();
+            ],
+        ];
+        $curl->setData($data);
+        $temp = $curl->getWithData();
 
-            if (isset($temp["error"])) {
-                return ["search_element"=> $this->keyWord,"docs" => []]; 
-            }
+        if (isset($temp["error"])) {
+            return ["search_element" => $this->keyWord, "docs" => []];
+        }
 
-            if ($temp["took"] == 0) {
-                return ["search_element"=> $this->source,"docs" => []];
-            }
+        if ($temp["took"] == 0) {
+            return ["search_element" => $this->source, "docs" => []];
+        }
+//            echo "<pre>";            
+//            print_r($temp);
+//            echo "</pre>";
+
+        foreach ($temp["hits"]["hits"] as $key => $element) {
+            $arr = [];
             $arr["path"] = str_replace(
                     "trunk/docs",
                     "knowledgebasedoc",
-                    trim($temp["hits"]["hits"][0]["_source"]["path"]["virtual"], "/")
+                    trim($element["_source"]["path"]["virtual"], "/")
             );
-            if (isset($temp["hits"]["hits"][0]["_source"]["meta"]["title"])) {
-                $arr["name"] = $temp["hits"]["hits"][0]["_source"]["meta"]["title"];
+            if (isset($element["_source"]["meta"]["title"])) {
+                $arr["name"] = $element["_source"]["meta"]["title"];
             } else {
-                $arr["name"] = $temp["hits"]["hits"][0]["_source"]["file"]["filename"];
+                $arr["name"] = $element["_source"]["file"]["filename"];
             }
-            $arr["content"] = implode("<br/>", $temp["hits"]["hits"][0]["highlight"]["content"]);
-            $result["doc"] = $arr;
+            $arr["content"] = implode("<br/>", $element["highlight"]["content"]);
+            $result["doc"][] = $arr;
+        }
+//            exit(0);
 //        }
         return array(
-            "search_element"=> $this->keyWord,
+            "search_element" => $this->keyWord,
             "docs" => $result,
         );
     }
+
 }
