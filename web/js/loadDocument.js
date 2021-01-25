bdc95e12d26c684eaaa5b43d820fa5009c7c5122
Gudov Vladislav <vlad.gudov@inbox.ru>
2021-01-25 17:28:19 +0400

    отображение

--- web/js/loadDocument.js
+++ web/js/loadDocument.js
@@ -6,21 +6,86 @@ function getListDir(dir, parent) {
         json = JSON.parse(xhr.response);
         createStruct(json, parent);
     };
-    xhr.open('GET', "DocumentListLoad.php?dir-0="+dir, true);
+    xhr.open('GET', "DocumentListLoad.php?dir-0=" + dir, true);
     xhr.send();
 }
 
+function createVirtualDirs(obj) {
+    var virtual = {};
+    obj["content"].sort(function (a, b) {
+        if (a.title && b.title) {
+            return a.title.toLowerCase() > b.title.toLowerCase();
+        }
+    });
+    obj["content"].forEach(function (el, i) {
+        if (el.title) {
+            var titleSplit = el.title.split("|");
+            if (titleSplit.length > 1) {
+                var key = titleSplit[0].trim();
+                el.title = titleSplit[1].trim();
+                if (virtual[key]) {
+                    virtual[key].push(el);
+                } else {
+                    virtual[key] = [el];
+                }
+                obj["content"][i] = null;
+            }
+        }
+    });
+    
+    if (JSON.stringify(virtual) === JSON.stringify({})) {
+        return [];
+    }
+    
+    var items = [];
+    for (var key in virtual) {        
+        var item = document.createElementNS('http://www.w3.org/1999/xhtml', "div"),
+            icon = document.createElementNS('http://www.w3.org/1999/xhtml', "i"),
+            content = document.createElementNS('http://www.w3.org/1999/xhtml', "div"),
+            header = document.createElementNS('http://www.w3.org/1999/xhtml', "div");
+        item.className = "item";
+        icon.className = "icon outline folder open";
+        header.className = "pointer";
+        header.addEventListener("click", function (e) {
+            var list = e.target.parentNode.querySelector(".list");            
+            var icon = e.target.parentNode.parentNode.querySelector(".icon");
+
+            if (icon.classList.contains("open")) {
+                list.style.display = "none";
+            } else {
+                list.style.display = "block";
+            }
+            icon.classList.toggle("open");
+        });
+        content.className = "content";
+        header.className += " header";
+        header.innerHTML = key;
+        content.appendChild(header);
+        createStruct({"content": virtual[key]}, content);
+        item.appendChild(icon);
+        item.appendChild(content);        
+        items.push(item);
+    }
+    return items;
+}
 
 function createStruct(obj, parent) {
     // главный див который будет добавлен в parent 
     var main = document.createElementNS('http://www.w3.org/1999/xhtml', "div");
     main.className = "ui list scale_text";
+    var items = createVirtualDirs(obj, parent);
+    items.forEach(function (el) {
+        main.appendChild(el);
+    });
     // проходимся в цикле по всем элементам и добавляем каждый как отдельный item  
     // в main div
     obj["content"].forEach(function (e) {
+        if (e === null) {
+            return;
+        }
         var item = document.createElementNS('http://www.w3.org/1999/xhtml', "div"),
                 icon = document.createElementNS('http://www.w3.org/1999/xhtml', "i"),
-                content = document.createElementNS('http://www.w3.org/1999/xhtml',"div"),
+                content = document.createElementNS('http://www.w3.org/1999/xhtml', "div"),
                 header = document.createElementNS('http://www.w3.org/1999/xhtml', "div"),
                 span = document.createElementNS('http://www.w3.org/1999/xhtml', "span"),
                 name = "",
@@ -39,7 +104,7 @@ function createStruct(obj, parent) {
                 } else {
                     getListDir(name, content);
                 }
-                icon.classList.toggle("open");                
+                icon.classList.toggle("open");
             });
         } else {
             if (!e.parent) {
@@ -50,20 +115,20 @@ function createStruct(obj, parent) {
             } else {
                 name = e.name;
             }
-            var a = document.createElementNS('http://www.w3.org/1999/xhtml',"a");
+            var a = document.createElementNS('http://www.w3.org/1999/xhtml', "a");
             a.href = "DocumentView.php/" + e.parent + "/" + e.name;
             a.target = "_blank";
             a.innerHTML = name;
             link = a;
             icon.className += " file";
         }
-        
+
         if (link !== null) {
             span.appendChild(link);
         } else {
             span.innerHTML = name;
         }
-        
+
         content.className = "content";
         header.className += " header";
 
@@ -78,5 +143,5 @@ function createStruct(obj, parent) {
 }
 
 window.addEventListener('load', function () {
-   getListDir("", document.querySelector(".ui.container"));
+    getListDir("", document.querySelector(".ui.container"));
 });
